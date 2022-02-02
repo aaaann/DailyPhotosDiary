4609c58b709be372486c41f551481a4d1cb55bc4
Anna Terekhova <Terekhova.A.An@sberbank.ru>
2022-02-03 02:48:32 +0300

    флоу для получения ивентов выбора пункта в боттомшите

--- features/gallery/gallery-impl/src/main/java/com/annevonwolffen/gallery_impl/presentation/viewmodels/AddImageViewModel.kt
+++ features/gallery/gallery-impl/src/main/java/com/annevonwolffen/gallery_impl/presentation/viewmodels/AddImageViewModel.kt
@@ -7,12 +7,17 @@ import androidx.lifecycle.viewModelScope
 import com.annevonwolffen.gallery_impl.domain.Photo
 import com.annevonwolffen.gallery_impl.domain.PhotosInteractor
 import com.annevonwolffen.gallery_impl.domain.UploadImage
+import com.annevonwolffen.gallery_impl.presentation.AddImageBottomSheet.AddImage
 import com.annevonwolffen.gallery_impl.presentation.Result
 import com.annevonwolffen.gallery_impl.presentation.State
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import java.io.File
 
 class AddImageViewModel(private val imagesInteractor: PhotosInteractor) : ViewModel() {
 
@@ -20,28 +25,38 @@ class AddImageViewModel(private val imagesInteractor: PhotosInteractor) : ViewMo
         get() = _uriFlow
     private val _uriFlow: MutableStateFlow<Uri?> = MutableStateFlow(null)
 
-    val uploadedImageFlow: StateFlow<State<List<Photo>>>
-        get() = _uploadedImageFlow // TODO: change to channel (single event)
-    private val _uploadedImageFlow: MutableStateFlow<State<List<Photo>>> = MutableStateFlow(State.Success(emptyList()))
+    var file: File? = null
+    var uri: Uri? = null
+
+    val uploadedImageFlow get() = _uploadedImageFlow.receiveAsFlow()
+    private val _uploadedImageFlow = Channel<State<List<Photo>>>(CONFLATED)
+
+    val addImageEvent get() = _addImageEvent.receiveAsFlow()
+    private val _addImageEvent = Channel<AddImage>(CONFLATED)
 
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         Log.w(TAG, "Ошибка при загрузке картинок: $throwable")
     }
 
-    fun setUri(uri: Uri?) {
-        _uriFlow.value = uri
-    }
+    // fun setUri(uri: Uri?) {
+    //     _uriFlow.value = uri
+    // }
 
     fun saveImage(image: UploadImage) {
         viewModelScope.launch(exceptionHandler) {
             imagesInteractor.uploadImages(TEST_FOLDER, image).also {
-                _uploadedImageFlow.value =
-                    when(it) {
+                _uploadedImageFlow.send(
+                    when (it) {
                         is Result.Success -> State.Success(it.value)
                         is Result.Error -> State.Error(it.errorMessage)
                     }
+                )
             }
         }
+    }
+
+    fun addImage(addImageCommand: AddImage) {
+        viewModelScope.launch { _addImageEvent.send(addImageCommand) }
     }
 
     private companion object {
