72031c9d2e4a63c410df268250a8df7a2fdeb554
Anna Terekhova <Terekhova.A.An@sberbank.ru>
2022-02-20 01:53:51 +0300

    удаление изображения из Firebase Cloud Storage

--- features/gallery/gallery-impl/src/main/java/com/annevonwolffen/gallery_impl/domain/ImagesRepository.kt
+++ features/gallery/gallery-impl/src/main/java/com/annevonwolffen/gallery_impl/domain/ImagesRepository.kt
@@ -7,5 +7,6 @@ interface ImagesRepository {
     fun loadImages(folder: String): Flow<Result<List<Image>>>
     suspend fun uploadImageToDatabase(folder: String, image: Image): Result<String>
     suspend fun uploadFileToStorage(folder: String, image: Image)
-    suspend fun deleteImage(folder: String, image: Image): Result<Unit>
+    suspend fun deleteImageFromDatabase(folder: String, image: Image): Result<Unit>
+    suspend fun deleteFileFromStorage(folder: String, image: Image)
 }
