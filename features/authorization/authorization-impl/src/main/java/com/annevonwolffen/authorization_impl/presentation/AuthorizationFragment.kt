ac87da85f93cbd2febc83c05bf1252f619ac9ebe
Anna Terekhova <Terekhova.A.An@sberbank.ru>
2022-03-10 22:57:46 +0300

    фрагмент авторизации разделен на фрагмент входа и фрагмент создания аккаунта, поправлена верстка

--- features/authorization/authorization-impl/src/main/java/com/annevonwolffen/authorization_impl/presentation/AuthorizationFragment.kt
+++ features/authorization/authorization-impl/src/main/java/com/annevonwolffen/authorization_impl/presentation/AuthorizationFragment.kt
@@ -4,112 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.FrameLayout
-import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.annevonwolffen.authorization_api.di.AuthorizationApi
-import com.annevonwolffen.authorization_api.domain.AuthState
-import com.annevonwolffen.authorization_api.domain.Loading
-import com.annevonwolffen.authorization_api.domain.NotSignedIn
-import com.annevonwolffen.authorization_api.domain.SignedIn
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.annevonwolffen.authorization_impl.R
-import com.annevonwolffen.coroutine_utils_api.extension.launchFlowCollection
-import com.annevonwolffen.di.FeatureProvider.getFeature
-import com.annevonwolffen.navigation.activityNavController
-import com.annevonwolffen.navigation.navigateSafely
-import com.annevonwolffen.ui_utils_api.extensions.setVisibility
-import kotlinx.coroutines.launch
-import com.annevonwolffen.navigation.R as NavigationR
 
 class AuthorizationFragment : Fragment() {
 
-    private lateinit var loadingScreen: FrameLayout
-
-    private val viewModel: AuthorizationViewModel by viewModels {
-        object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return AuthorizationViewModel(getFeature(AuthorizationApi::class.java).authInteractor) as T
-            }
-        }
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_authorization, container, false)
+        return inflater.inflate(R.layout.fragment_auth, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        observeAuthorizationFlows()
 
-        view.findViewById<Button>(R.id.btn_sign_in).setOnClickListener {
-            signIn(view)
-        }
+        val navHostFragment =
+            childFragmentManager.findFragmentById(R.id.nav_host_fragment_auth) as NavHostFragment? ?: return
 
-        view.findViewById<Button>(R.id.btn_no_account).setOnClickListener {
-            it.visibility = View.GONE
-            view.findViewById<Button>(R.id.btn_sign_in).visibility = View.GONE
-            view.findViewById<Button>(R.id.btn_sign_up).visibility = View.VISIBLE
-            view.findViewById<Button>(R.id.btn_has_account).visibility = View.VISIBLE
-        }
-
-        view.findViewById<Button>(R.id.btn_has_account).setOnClickListener {
-            it.visibility = View.GONE
-            view.findViewById<Button>(R.id.btn_sign_in).visibility = View.VISIBLE
-            view.findViewById<Button>(R.id.btn_sign_up).visibility = View.GONE
-            view.findViewById<Button>(R.id.btn_no_account).visibility = View.VISIBLE
-        }
-
-        view.findViewById<Button>(R.id.btn_sign_up).setOnClickListener {
-            signUp(view)
-        }
-
-        loadingScreen = view.findViewById(R.id.progress_layout)
-    }
-
-    private fun observeAuthorizationFlows() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launchFlowCollection(viewModel.authFlow) { handleAuth(it) }
-
-                launchFlowCollection(viewModel.authorizationFailure) {
-                    Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    private fun signIn(view: View) {
-        val email = view.findViewById<EditText>(R.id.et_email).text.toString()
-        val password = view.findViewById<EditText>(R.id.et_password).text.toString()
-        viewModel.signIn(email, password)
-    }
-
-    private fun signUp(view: View) {
-        val email = view.findViewById<EditText>(R.id.et_email).text.toString()
-        val password = view.findViewById<EditText>(R.id.et_password).text.toString()
-        viewModel.signUp(email, password)
-    }
-
-    private fun handleAuth(authState: AuthState) {
-        when (authState) {
-            is SignedIn -> {
-                activityNavController().navigateSafely(NavigationR.id.action_global_mainScreen)
-                loadingScreen.setVisibility(false)
-            }
-            is NotSignedIn -> {
-                loadingScreen.setVisibility(false)
-            }
-            is Loading -> {
-                loadingScreen.setVisibility(true)
-            }
-        }
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        view.findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
