ad3c0663e95eede7f26b0f9418c4faab6efe05dc
Anna Terekhova <Terekhova.A.An@sberbank.ru>
2022-03-17 23:01:13 +0300

    main screen ui test

--- app/src/androidTest/java/com/annevonwolffen/dailyphotosdiary/screens/MainScreen.kt
+++ app/src/androidTest/java/com/annevonwolffen/dailyphotosdiary/screens/MainScreen.kt
@@ -1,14 +1,12 @@
 package com.annevonwolffen.dailyphotosdiary.screens
 
-import com.kaspersky.kaspresso.screens.KScreen
 import com.annevonwolffen.mainscreen_impl.R
-import com.annevonwolffen.navigation.R as NavR
 import com.annevonwolffen.mainscreen_impl.presentation.MainScreenFragment
+import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.drawer.KDrawerView
 import io.github.kakaocup.kakao.navigation.KNavigationView
-import com.annevonwolffen.mainscreen_impl.presentation.MainScreenFragment
-import io.github.kakaocup.kakao.drawer.KDrawerView
 import io.github.kakaocup.kakao.toolbar.KToolbar
+import com.annevonwolffen.navigation.R as NavR
 
 object MainScreen : KScreen<MainScreen>() {
     override val layoutId: Int? = R.layout.fragment_main_screen
