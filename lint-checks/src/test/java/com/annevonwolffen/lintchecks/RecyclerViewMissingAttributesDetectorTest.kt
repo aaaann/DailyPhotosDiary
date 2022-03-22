5d54ae7a4422c33db1c53f1e52da40cc87ec6530
Anna Terekhova <Terekhova.A.An@sberbank.ru>
2022-03-22 20:25:20 +0300

    линт правило и тесты

--- lint-checks/src/test/java/com/annevonwolffen/lintchecks/RecyclerViewMissingAttributesDetectorTest.kt
+++ lint-checks/src/test/java/com/annevonwolffen/lintchecks/RecyclerViewMissingAttributesDetectorTest.kt
@@ -2,7 +2,9 @@ package com.annevonwolffen.lintchecks
 
 import com.android.tools.lint.checks.infrastructure.LintDetectorTest.xml
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import com.annevonwolffen.lintchecks.TestRecyclerViewMissingAttributesDetectorDataProvider.getLayoutFileWithRecyclerViewWithOverScrollModeAlways
 import com.annevonwolffen.lintchecks.TestRecyclerViewMissingAttributesDetectorDataProvider.getLayoutFileWithRecyclerViewWithOverScrollModeNever
+import com.annevonwolffen.lintchecks.TestRecyclerViewMissingAttributesDetectorDataProvider.getLayoutFileWithRecyclerViewWithoutOverScrollMode
 import org.junit.Test
 
 @Suppress("UnstableApiUsage")
@@ -10,17 +12,62 @@ class RecyclerViewMissingAttributesDetectorTest {
 
     @Test
     fun `recycler view without overScrollMode attribute`() {
+        lint()
+            .allowMissingSdk()
+            .files(
+                xml(
+                    "res/layout/layout.xml",
+                    getLayoutFileWithRecyclerViewWithoutOverScrollMode()
+                )
+            )
+            .issues(RecyclerViewMissingAttributesDetector.OVERSCROLL_MODE_NEVER_ISSUE)
+            .run()
+            .expect(
+                """
+                res/layout/layout.xml:12: Warning: 
+                            Отсутствует атрибут android:overScrollMode со значением never
+                         [MissingOverScrollModeNever]
+                        <androidx.recyclerview.widget.RecyclerView
+                        ^
+                0 errors, 1 warnings
+            """.trimIndent()
+            )
     }
 
     @Test
     fun `recycler view with incorrect overScrollMode attribute`() {
+        lint()
+            .allowMissingSdk()
+            .files(
+                xml(
+                    "res/layout/layout.xml",
+                    getLayoutFileWithRecyclerViewWithOverScrollModeAlways()
+                )
+            )
+            .issues(RecyclerViewMissingAttributesDetector.OVERSCROLL_MODE_NEVER_ISSUE)
+            .run()
+            .expect(
+                """
+                res/layout/layout.xml:19: Warning: 
+                            Отсутствует атрибут android:overScrollMode со значением never
+                         [MissingOverScrollModeNever]
+                            android:overScrollMode="always"
+                            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                0 errors, 1 warnings
+            """.trimIndent()
+            )
     }
 
     @Test
     fun `recycler view with overScrollMode never`() {
         lint()
             .allowMissingSdk()
-            .files(xml("res/layout/layout.xml", getLayoutFileWithRecyclerViewWithOverScrollModeNever()))
+            .files(
+                xml(
+                    "res/layout/layout.xml",
+                    getLayoutFileWithRecyclerViewWithOverScrollModeNever()
+                )
+            )
             .issues(RecyclerViewMissingAttributesDetector.OVERSCROLL_MODE_NEVER_ISSUE)
             .run()
             .expect("No warnings.")
