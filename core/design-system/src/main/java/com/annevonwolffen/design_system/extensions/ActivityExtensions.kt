1dfa20bd9b67adbea9356e7e757a70420c5f2588
Anna Terekhova <Terekhova.A.An@sberbank.ru>
2022-03-29 02:23:32 +0300

    hide keyboard

--- core/design-system/src/main/java/com/annevonwolffen/design_system/extensions/ActivityExtensions.kt
+++ core/design-system/src/main/java/com/annevonwolffen/design_system/extensions/ActivityExtensions.kt
@@ -1,6 +1,8 @@
 package com.annevonwolffen.design_system.extensions
 
 import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.ColorRes
 
 fun Activity.removeNavBarInset(listener: OnSystemInsetsChangedListener = { _, _ -> }) {
@@ -9,4 +11,11 @@ fun Activity.removeNavBarInset(listener: OnSystemInsetsChangedListener = { _, _ 
 
 fun Activity.setStatusBarColor(@ColorRes colorId: Int) {
     window.statusBarColor = resources.getColor(colorId, theme)
+}
+
+fun Activity.hideKeyboard() {
+    currentFocus?.let { view ->
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
